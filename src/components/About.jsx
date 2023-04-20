bb357fa2fac263d8674847c519e3b9e1b3222b1c
Meghann Stamper <meg.stamp@yahoo.com>
2023-04-20 19:58:12 -0400

    easter eggs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,46 @@
-import { Container, Row, Col } from "react-bootstrap"
-
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useState } from "react";
 
 export default function About() {
+  const [picToggle, setPicToggle] = useState(false);
+  const [textToggle, setTextToggle] = useState(false);
 
-    return (
-        <main>
-            <Container className="text-center">
-                <Row className="aboutrow">
-                    <Col sm={12} md={6}>
-                        <img
-                        src="/images/headshot-square.jpg"
-                        className="rounded-circle"
-                        alt="Profile Picture"
-                        />
-                    </Col>
+  return (
+    <main>
+      <Container className="text-center">
+        <Row className="aboutrow">
+          <Col sm={12} md={6}>
+            <img
+              onClick={() => setPicToggle(!picToggle)}
+              src={
+                picToggle ? "/images/pic1.jpg" : "/images/headshot-square-1.jpg"
+              }
+              className="rounded-circle"
+              alt="Meghann Stamper"
+            />
+          </Col>
 
-                    <Col> 
-                    <h1>Meghann Stamper</h1>
-                    <p> I spent most of my life avoiding things that I deemed “scary,” or “too difficult;” in doing so, I missed many opportunities - academic, social, and career-based. When I turned forty I decided I did not want to live my life in fear any more. So here I am, ready to embark on a scary, difficult, thrilling, creative, yet rewarding new new career path!</p>
-                    </Col>
-                </Row>
-            </Container>
-        </main>
-
-    )
+          <Col>
+            <h1>Meghann Stamper</h1>
+            {textToggle
+                ? <p>Fun Fact: I have freckles on my lips!</p>
+                : <p>
+              I spent most of my life avoiding things that I deemed “scary,” or
+              “too difficult;” in doing so, I missed many opportunities -
+              academic, social, and career-based. When I turned forty I decided
+              I did not want to live my life in fear any more. So here I am,
+              ready to embark on a scary, difficult, thrilling, creative, yet
+              rewarding new new career path!
+            </p>}
+            <Button
+              onClick={() => setTextToggle(!textToggle)}
+              className="button"
+            >
+              Click Me{" "}
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  );
 }
